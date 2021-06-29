d49396e2a55da705425befdc676e65d91eb50686
brandonricharda <brandonricharda@gmail.com>
2021-06-28 22:03:20 -0400

    Add task class

---
+++ src/task.js
@@ -0,0 +1,9 @@
+class Task {
+    constructor(name, description, dueDate, priority, status) {
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.status = status;
+    }
+}
