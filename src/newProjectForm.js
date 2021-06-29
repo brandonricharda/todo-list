f8161c4bb936dbac877066aad858fbe55e4547c5
brandonricharda <brandonricharda@gmail.com>
2021-06-29 15:13:50 -0400

    Begin converting styles into framework

--- src/newProjectForm.js
+++ src/newProjectForm.js
@@ -11,10 +11,12 @@ const newProjectForm = (function() {
             // create new project form elements
             let projectNameInput = document.createElement("input");
             projectNameInput.setAttribute("id", "project-name-input");
+            projectNameInput.setAttribute("class", "flex-1");
             projectNameInput.setAttribute("placeholder", "Project Name");
             let projectSubmitButton = document.createElement("button");
             projectSubmitButton.innerHTML = "Create";
             projectSubmitButton.setAttribute("id", "project-submit-button");
+            projectSubmitButton.setAttribute("class", "flex-1 flat-padded-clickable-button is-green");
             // add new project form elements to DOM
             projectFormContainer.appendChild(projectNameInput);
             projectFormContainer.appendChild(projectSubmitButton);
